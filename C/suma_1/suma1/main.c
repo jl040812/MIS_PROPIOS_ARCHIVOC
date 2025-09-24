521cbc5c0f9ca332e502da490118f48e82fa1d3f
jl040812 <jldiazb@udistrital.edu.co>
2025-09-24 09:52:15 -0500

    Add files via upload

---
+++ C/suma_1/suma1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int a, b, r;
+
+int main() {
+
+	a=5;
+	b=7;
+	r=0;
+	
+	r=a+b;
+	printf("Resultado: %d\n", r);
+	
+	
+
+	return 0;
+}
